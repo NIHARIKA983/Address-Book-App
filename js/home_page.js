1b7647106fdd4414478cb90d39e7d1010051ab72
NIHARIKA983 <niharikagowda1996@gmail.com>
2021-09-03 16:34:16 +0530

    Added:Populate the AddressBook List From the LocalStorage

--- js/home_page.js
+++ js/home_page.js
@@ -1,13 +1,23 @@
+//declare global variable for employee payroll list
+let addressbookList;
+
 window.addEventListener('DOMContentLoaded', () => {
+    addressbookList = getPersonDataFromLocalStorage();
+    document.querySelector('.person-count').textContent = addressbookList.length;
     createInnerHTML();
 });
+//get list of Person data from local storage
+const getPersonDataFromLocalStorage = () => {
+    return localStorage.getItem('AddressBookList') ?
+                        JSON.parse(localStorage.getItem('AddressBookList')) : [] ;
+}
 
 //Template literal ES6 feature 
 const createInnerHTML = () => {
     const headerHtml = "<th>Full Name</th><th>Address</th><th>City</th><th>State</th>"+
                        "<th>ZipCode</th><th>Phone Number</th><th>Actions</th>";
+    if( addressbookList.length == 0 ) return;
     let innerHtml = `${headerHtml}`;
-    let addressbookList = createAddressBookJSON();
 
     for  ( const addressbookData of addressbookList) {
         innerHtml = `${innerHtml}
@@ -19,36 +29,11 @@ const createInnerHTML = () => {
             <td>${addressbookData._zipCode}</td>
             <td>${addressbookData._phoneNumber}</td>
             <td>
-                <img name="${addressbookData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                <img name="${addressbookData._id}" alt="Edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
+                <img id="${addressbookData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                <img id="${addressbookData._id}" alt="Edit" onclick="update(this)" src="../assets/icons/create-black-18dp.svg">
             </td>        
         </tr>
         `;
     }
     document.querySelector('#table-display').innerHTML=innerHtml;
 }
-
-//create Employee Payroll JSON objects
-const createAddressBookJSON = () => {
-    let addressbookList = [
-        {
-            _name : 'Niharika',
-            _address : 'Balehonnur,N.R.Pura(tq)',
-            _city : 'Chikmagalur',
-            _state : 'Karnataka',
-            _zipCode : '591118',
-            _phoneNumber : '+918277587802',
-            _id : new Date().getTime()
-        },
-        {
-            _name : 'Srinivas',
-            _address : 'Balehonnur,N.R.Pura(tq)',
-            _city : 'Chikmagalur',
-            _state : 'Karnataka',
-            _zipCode : '591118',
-            _phoneNumber : '+918277587802',
-            _id : new Date().getTime()
-        }
-    ];
-    return addressbookList;
-} 
